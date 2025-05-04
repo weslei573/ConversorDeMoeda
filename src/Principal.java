0136424b86cfb463ff7984e92304e344fef58336
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2025-05-03 21:14:58 -0400

    Interagindo com o usario

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int operado;
+        String menuSair = """
+                (1) 
+                (0)
+                """;
         String menu = """
                 *************************************************************************
                 SEJA BEM-VINDO/A AO CONVERSOR DE MOEDA
@@ -15,16 +20,21 @@ public class Principal {
                                 TWD	USD	ZAR
                 *************************************************************************
                 """;
-        System.out.println(menu);
-        System.out.println("CONVERTE DE: ");
-        var base_case = scanner.nextLine();
-        System.out.println("PARA: ");
-        var target_code = scanner.nextLine();
-        System.out.println("VALOR: ");
-        double valor = scanner.nextInt();
+        do {
+            System.out.println(menu);
+            System.out.println("CONVERTE DE: ");
+            var base_case = scanner.nextLine();
+            System.out.println("PARA: ");
+            var target_code = scanner.nextLine();
+            System.out.println("VALOR: ");
+            double valor = scanner.nextInt();
 
-        ConsultaMoeda consultaMoeda = new ConsultaMoeda();
-        Conversor valorMoeda = consultaMoeda.buscaConversorMoeda(base_case, target_code);
-        System.out.println(valorMoeda.conversaoEmMoedaLocal(valor));
+            ConsultaMoeda consultaMoeda = new ConsultaMoeda();
+            Conversor valorMoeda = consultaMoeda.buscaConversorMoeda(base_case, target_code);
+            System.out.println(valorMoeda.conversaoEmMoedaLocal(valor));
+            System.out.println("Para finaliza digite (1) para continuar (0)");
+            operado = scanner.nextInt();
+        } while (operado!=1);
+        System.out.println("Operação finalizada");
     }
 }
